1d76f6641c622b87a7d38877481a740efa2254b3
Pisharody <VPISHAR@emory.edu>
2024-03-08 14:41:07 -0500

    Setup file to use r code on the "special softare" computer in woodruff library (has spss).

---
+++ setup.r
@@ -0,0 +1,41 @@
+# Set up library computer for this project
+
+# Need to install git each time :(
+# https://git-scm.com/download/win
+# Seems ok though? 
+
+# Install packages
+p = c(
+  "broom", 
+  "dbplyr", 
+  "dtplyr", 
+  "forcats", 
+  "googledrive", 
+  "googlesheets4", 
+  "hms",
+  "modelr",
+  "pillar",
+  "reprex", 
+  "rvest",
+  "stringr",
+  "tidyr", 
+  "dplyr", 
+  "tidyverse", 
+  "purrr", 
+  "readr", 
+  "ggplot2",
+  "gitcreds")
+install.packages(p)
+
+# Set up gitcreds
+gitcreds_set(url = "https://github.com/vapisharody")
+
+# Check out this repo
+# https://github.com/vapisharody/eoa
+# to THIS location
+# C:/Users/vpishar/Desktop
+
+
+
+##
+             
